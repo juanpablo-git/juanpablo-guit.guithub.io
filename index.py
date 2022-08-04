6fb43bc30b26a77abd3488155ae17e0e9989ab8e
juanpablo-git <salesjuan781@gmail.com>
2022-08-04 20:04:26 -0300

    escript fatur

---
+++ index.py
@@ -0,0 +1,46 @@
+import pyautogui as py
+import time
+
+py.press("win")
+time.sleep(1)
+py.write("firefox")
+time.sleep(1)
+py.press("enter")
+time.sleep(1)
+py.hotkey('ctrl', 'l')
+time.sleep(1)
+py.write("https://mail.google.com/mail/u/0/#search/cart%C3%B5es+caixa")
+time.sleep(1)
+py.press("enter")
+time.sleep(5)
+py.moveTo(600, 340)  
+time.sleep(1)
+py.click()
+time.sleep(1)
+py.moveTo(626,428)  
+time.sleep(2)
+py.click()
+py.write("08112850739")
+time.sleep(2)
+py.press("enter")
+time.sleep(5)
+print(py.position())
+py.moveTo(1288,144)  
+py.click()
+time.sleep(2)
+py.press("enter")
+time.sleep(2)
+py.press("enter")
+time.sleep(1)
+py.hotkey('ctrl', 't')
+time.sleep(1)
+py.hotkey('ctrl', 'l')
+py.write("web.whatsapp.com/")
+time.sleep(1)
+py.press("enter")
+
+
+
+
+
+
