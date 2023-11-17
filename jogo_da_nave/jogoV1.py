6a03b69f7fe82856926015702bd613ac82c2f6a4
juanpablo-git <salesjuan781@gmail.com>
2023-11-17 14:42:55 -0300

    -

--- jogo_da_nave/jogoV1.py
+++ jogo_da_nave/jogoV1.py
@@ -1,13 +1,29 @@
 import turtle
 import random
 
-atualiza =  True
-direction = "left"
+game_start = True
 
 window = turtle.Screen()
 window.bgcolor("green")
 window.title("Space Invaders")
 turtle.bgpic("background.gif")
+
+# def write_window(x,y,text,align="center", font=("Arial", 30, "normal")):
+#     game_over = turtle.Turtle()
+#     game_over.speed(0)
+#     game_over.color("red")
+#     game_over.penup()
+#     game_over.setposition(x, y)
+#     game_over.write(text, False, align, font)
+#     game_over.hideturtle()
+#     return game_over
+
+
+def click_restar(x,y):
+    print(x)
+
+# w = write_window(0,100,"restart")
+# w.onclick(click_restar)
 
 border_pen = turtle.Turtle()
 border_pen.speed(0)
@@ -16,6 +32,23 @@ border_pen.penup()
 setposition = border_pen.setposition(-300, -300)
 border_pen.pendown()
 border_pen.pensize(3)
+
+
+# Set the score to 0
+score = 0
+
+# Draw the pen
+score_pen = turtle.Turtle()
+score_pen.speed(0)
+score_pen.color("red")
+score_pen.penup()
+score_pen.setposition(-290, 270)
+scorestring = "SCORE: %s" % score
+score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+# score_pen.hideturtle()
+score_pen.showturtle()
+score_pen.onclick(lambda x,y : print(x))
+
 
 for side in range(4):
     border_pen.fd(600)
@@ -26,14 +59,13 @@ border_pen.hideturtle()
 turtle.register_shape("player.gif")
 turtle.register_shape("invader.gif")
 
-
 player = turtle.Turtle()
 player.shape("player.gif")
 player.penup()
 player.speed(0)
 player.setposition(0, -200)
 
-numbers_of_invaders = 10
+numbers_of_invaders = 11
 invaders = []
 
 for i in range(numbers_of_invaders):
@@ -50,18 +82,6 @@ for enemy in invaders:
 
 enemy.hideturtle()
 
-def create_fire(x):
-    fire = turtle.Turtle(visible=False)
-    fire.color("yellow")
-    fire.shape("triangle")
-    fire.penup()
-    fire.speed(0)
-    fire.setheading(90)
-    fire.shapesize(0.5, 0.5)
-    fire.setposition(x, -185)   
-    fire.showturtle()
-    return fire 
-
 fire = turtle.Turtle()
 fire.color("yellow")
 fire.shape("triangle")
@@ -71,210 +91,141 @@ fire.setheading(90)
 fire.shapesize(0.5, 0.5)
 fire.setposition(0, -185)
 
-
-firespeed = 15
+firespeed = 40
 playerspeed = 15
 fire_on = False
-
-def atualiza_invazor(interval = -1):
-    global  atualiza
-    global direction
-    frame_atual = 0
-    positions = []
-
-
-    for pos_invaders in invaders:
-        position = pos_invaders.xcor()
-        positions.append(position)
-
-    left_pos = min(positions)
-    right_pos = max(positions)
-
-    left_index = positions.index(left_pos)
-    right_index = positions.index(right_pos)
-
-    speedinvaders = 10
-    
-    
-    if interval > frame_atual:
-        frame_atual += 1
-    elif frame_atual == 1:
-        atualiza = False
-
-    while atualiza: 
-        if frame_atual == 1:
-            atualiza = False
-        if direction == "left":
-            
-            x_left = invaders[left_index].xcor()
-            if x_left >= -280:
-                for pos_invaders in invaders:
-                    position = pos_invaders.xcor()
-                    position -= speedinvaders
-                    pos_invaders.setx(position)
-
-                x_left -= speedinvaders
-
-            else:
-                for pos_invaders in invaders:
-                    position = pos_invaders.ycor()
-                    position -= 20
-                    pos_invaders.sety(position)
-                direction = "right"
-
-        elif direction == "right":
-            
-            x_right = invaders[right_index].xcor()
-            if x_right < 280:
-                for pos_invaders in invaders:
-                    position = pos_invaders.xcor()
-                    position += speedinvaders
-                    pos_invaders.setx(position)
-
-                x_right += speedinvaders
-            else:
-                for pos_invaders in invaders:
-                    position = pos_invaders.ycor()
-                    position -= 20
-                    pos_invaders.sety(position)
-                direction = "left"
-
+x_fire = 0
+y_fire = 0
 
 def move_fire():
-    global atualiza 
-    global fire_on
-    atualiza_fire = True
-    atualiza = False
-    if not fire_on:
-        fire_on = True
- 
-        fire_atual = create_fire(player.xcor())
-        
-        while  atualiza_fire:
-            for pos_invaders in invaders:
-                fire_atual_x = fire_atual.xcor()
-                fire_atual_y = fire_atual.ycor()
-                print(fire_atual_y,"-",pos_invaders.ycor(),"//",abs(abs(fire_atual_y) - abs(pos_invaders.ycor())))
-                if (abs(fire_atual_x - pos_invaders.xcor()) in [1,2,3,4,5,6,7,8,9,10] or fire_atual_x  == pos_invaders.xcor() ) and (abs(fire_atual_y - pos_invaders.ycor()) in [1,2,3,4,5,6,7,8,9,10] or fire_atual_y  == pos_invaders.ycor() ):
-                    pos_invaders.hideturtle()
-                    print("morreu")
-                # if abs(fire_atual_y - pos_invaders.ycor()) in [1,2,3,4,5,6,7,8,9,10] or fire_atual_y  == pos_invaders.ycor() :
-                #     pos_invaders.hideturtle()
-                #     print("morreu")
+    global fire_on, x_fire, y_fire,score
+    fire_on = True
+    y_fire =fire.ycor()
+    x_fire = fire.xcor()
 
-               
+    while y_fire < 300:
+        for inimigo in invaders:
+            x_ini = inimigo.xcor()
+            y_ini = inimigo.ycor()
+            if x_fire >= x_ini -20 and x_fire <= x_ini + 20:
+                if y_fire >= y_ini:
+                    global left_index, right_index, down_index
+                    inimigo.setx(random.randint(-280, 280))
+                    inimigo.sety(random.randint(100, 250))
+                    y_fire = 400
+                    left_index, right_index, down_index = init_pos()
+                    score += 10
+                    score_pen.clear()
+                    score_pen.write("Score: "+str(score), False, align="left", font=("Arial", 14, "normal"))
+                    break
+        y_fire += firespeed
+        fire.sety(y_fire)
 
-            
-                    
-            atualiza = False
-            if(fire_atual.ycor()  >= 300):
-                atualiza_fire = False            
-            fire_atual.sety(fire_atual.ycor() +15)
-
-           
-
-
-                   
-            atualiza = True
-            atualiza_invazor(1)
-        atualiza = True
-        fire_atual.hideturtle()
-        fire_on = False
-    # global fire_on
-    # global atualiza
-    # atualiza = False
-    # fire_on = True
-    # y = fire.ycor()
-    # while y < 300:
-    #     y += firespeed
-    #     fire.sety(y)
-    #     atualiza = True
-    #     atualiza_invazor(1)
-    #     atualiza = False
-    # atualiza = True
-
-    # fire.hideturtle()
-    # fire.sety(-185)
-    # fire_on = False
-    # fire.setx(player.xcor())
-    # fire.showturtle()
-
+    fire.hideturtle()
+    fire.sety(-185)
+    fire_on = False
+    fire.setx(player.xcor())
+    fire.showturtle()
 
 def move_right():
-    x = player.xcor()
-    x += playerspeed
-    if x > 280:
-        x = 280
-    player.setx(x)
-    # if not fire_on:
-    fire.setx(x)
-
+    if game_start:
+        x = player.xcor()
+        x += playerspeed
+        if x > 280:
+            x = 280
+        player.setx(x)
+        if not fire_on:
+            fire.setx(x)
 
 def move_left():
-    x = player.xcor()
-    x -= playerspeed
-    if x < -280:
-        x = -280
-    player.setx(x)
-    # if not fire_on:
-    fire.setx(x)
-
+    if game_start:
+        x = player.xcor()
+        x -= playerspeed
+        if x < -280:
+            x = -280
+        player.setx(x)
+        if not fire_on:
+            fire.setx(x)
 
 turtle.listen()
-turtle.onkey(move_fire,"space")
+turtle.onkey(move_fire, "space")
 turtle.onkey(move_right, "Right")
 turtle.onkey(move_left, "Left")
 
-atualiza_invazor()
+def init_pos():
+    x_positions = []
+    y_positions = []
+    for pos_invaders in invaders:
+        x_position = pos_invaders.xcor()
+        y_position = pos_invaders.ycor()
+        x_positions.append(x_position)
+        y_positions.append(y_position)
+
+    left_pos = min(x_positions)
+    right_pos = max(x_positions)
+
+    down_pos = min(y_positions)
+
+    left_id = x_positions.index(left_pos)
+    right_id = x_positions.index(right_pos)
+    down_id = y_positions.index(down_pos)
+
+    return left_id, right_id, down_id
+
+left_index, right_index, down_index = init_pos()
 
 
-turtle.mainloop()
+speedinvaders = 10
+direction = "left"
 
+while game_start:
+    if direction == "left":
+        x_left = invaders[left_index].xcor()
+        y = invaders[left_index].ycor()
+        if x_left >= -280:
+            for pos_invaders in invaders:
+                position = pos_invaders.xcor()
+                position -= speedinvaders
+                pos_invaders.setx(position)
+            x_left -= speedinvaders
+        else:
+            y -= 40
+            direction = "right"
+            for pos_invaders in invaders:
+                position = pos_invaders.ycor()
+                position -= 40
+                pos_invaders.sety(position)
 
-# asyncio.run(atualiza_invazor())
+    elif direction == "right":
+        x_right = invaders[right_index].xcor()
+        if x_right < 280:
+            for pos_invaders in invaders:
+                position = pos_invaders.xcor()
+                position += speedinvaders
+                pos_invaders.setx(position)
+            x_right += speedinvaders
+        else:
+            y -= 40
+            direction = "left"
+            for pos_invaders in invaders:
+                position = pos_invaders.ycor()
+                position -= 40
+                pos_invaders.sety(position)
 
-# positions = []
+    y_down = invaders[down_index].ycor()
+    if y_down <= -280:
+        game_start = False
+        break
 
-
-# for pos_invaders in invaders:
-#     position = pos_invaders.xcor()
-#     positions.append(position)
-
-# left_pos = min(positions)
-# right_pos = max(positions)
-
-# left_index = positions.index(left_pos)
-# right_index = positions.index(right_pos)
-
-# speedinvaders = 10
-# direction = "left"
-# while True: 
-#     if direction == "left":
-#         x_left = invaders[left_index].xcor()
-#         if x_left >= -280:
-#             for pos_invaders in invaders:
-#                 position = pos_invaders.xcor()
-#                 position -= speedinvaders
-#                 pos_invaders.setx(position)
-
-#             x_left -= speedinvaders
-
-#         else:
-#             direction = "right"
-
-#     elif direction == "right":
-#         x_right = invaders[right_index].xcor()
-#         if x_right < 280:
-#             for pos_invaders in invaders:
-#                 position = pos_invaders.xcor()
-#                 position += speedinvaders
-#                 pos_invaders.setx(position)
-
-#             x_right += speedinvaders
-#         else:
-#             direction = "left"
+# Draw the pen
+game_over = turtle.Turtle()
+game_over.speed(0)
+game_over.color("red")
+game_over.penup()
+game_over.setposition(0, 0)
+game_over.write("Game Over", False, align="center", font=("Arial", 30, "normal"))
+game_over.hideturtle()
 
 
 
 turtle.done()
-
--- proj_prof/class_jogo.py
+++ proj_prof/class_jogo.py
@@ -13,6 +13,10 @@ class jogo():
     y_fire = 0
     fire = turtle.Turtle()
     player = turtle.Turtle()
+    direction = "left"
+    speedinvaders = 15
+    inviders_loop = True
+    score_pen = turtle.Turtle()
     def generate_fire(self):
         self.fire.color("yellow")
         self.fire.shape("triangle")
@@ -21,6 +25,7 @@ class jogo():
         self.fire.setheading(90)
         self.fire.shapesize(0.5, 0.5)
         self.fire.setposition(0, -185)
+        self.fire.showturtle()
         return self.fire
     def generate_player(self):
         turtle.register_shape("player.gif")
@@ -28,9 +33,12 @@ class jogo():
         self.player.penup()
         self.player.speed(0)
         self.player.setposition(0, -200)
+        self.player.showturtle()
         return self.player
     def index(self):
-
+        self.fire.hideturtle()
+        self.score_pen.hideturtle()
+        self.player.hideturtle()
         turtle.register_shape("invader.gif")
         turtle.register_shape("reload.gif")
 
@@ -39,6 +47,7 @@ class jogo():
         window.title("Space Invaders")
         turtle.bgpic("background.gif")
 
+
         border_pen = turtle.Turtle()
         border_pen.speed(0)
         border_pen.color("white")
@@ -46,74 +55,79 @@ class jogo():
         border_pen.setposition(-300, -300)
         border_pen.pendown()
         border_pen.pensize(3)
+        border_pen.hideturtle()
 
         for side in range(4):
             border_pen.fd(600)
             border_pen.lt(90)
 
         border_pen.hideturtle()
-
+        self.generate_inviders()
         self.generate_fire()
         self.generate_player()
+
 
         # Set the score to 0
         score = 0
 
         # Draw the pen
-        score_pen = turtle.Turtle()
-        score_pen.speed(0)
-        score_pen.color("red")
-        score_pen.penup()
-        score_pen.setposition(-290, 270)
+        self.score_pen = turtle.Turtle()
+        self.score_pen.speed(0)
+        self.score_pen.color("red")
+        self.score_pen.penup()
+        self.score_pen.setposition(-290, 270)
         scorestring = "SCORE: %s" % score
-        score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
-        score_pen.hideturtle()
+        self.score_pen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
+        self.score_pen.hideturtle()
 
         turtle.listen()
         turtle.onkey(lambda : self.move_fire(self.fire), "space")
         turtle.onkey(self.move_right, "Right")
         turtle.onkey(lambda  : self.move_left(self.player,self.fire), "Left")
+
+        self.move_inimigos()
+
         
         turtle.done()
 
 
     def refresh(self):
-        # global direction,invaders
+        invaders = self.invaders
         left_index, right_index, down_index = self.init_pos()
         y = self.invaders[left_index].ycor()
-        if direction == "left":
+        if self.direction == "left":
             x_left = invaders[left_index].xcor()
 
             if x_left >= -280:
-                for pos_invaders in invaders:
+                for pos_invaders in self.invaders:
                     position = pos_invaders.xcor()
-                    position -= speedinvaders
+                    position -= self.speedinvaders
                     pos_invaders.setx(position)
-                x_left -= speedinvaders
+                x_left -= self.speedinvaders
             else:
                 y -= 40
-                direction = "right"
-                for pos_invaders in invaders:
+                self.direction = "right"
+                for pos_invaders in self.invaders:
                     position = pos_invaders.ycor()
                     position -= 40
                     pos_invaders.sety(position)
 
-        elif direction == "right":
+        elif self.direction == "right":
             x_right = invaders[right_index].xcor()
             if x_right < 280:
-                for pos_invaders in invaders:
+                for pos_invaders in self.invaders:
                     position = pos_invaders.xcor()
-                    position += speedinvaders
+                    position += self.speedinvaders
                     pos_invaders.setx(position)
-                x_right += speedinvaders
+                x_right += self.speedinvaders
             else:
                 y -= 40
                 direction = "left"
-                for pos_invaders in invaders:
+                for pos_invaders in self.invaders:
                     position = pos_invaders.ycor()
                     position -= 40
                     pos_invaders.sety(position)
-    def write(x,y,text):
+    def write(self,x,y,text):
         game_over = turtle.Turtle()
         game_over.speed(0)
         game_over.color("red")
@@ -122,20 +136,26 @@ class jogo():
         game_over.write(text, False, align="center", font=("Arial", 30, "normal"))
         game_over.hideturtle()
         return game_over
-    def move_inimigos ():
-        global left_index, right_index, down_index,direction,invaders
-        inviders_loop = True
-        while inviders_loop:
-            left_index, right_index, down_index = init_pos()
+    def move_inimigos (self):
+        # global left_index, right_index, down_index,direction,invaders
+        invaders = self.invaders
+        inviders_loop = self.inviders_loop
+        direction = self.direction
+        while self.inviders_loop:
+            left_index, right_index, down_index = self.init_pos()
             y = invaders[left_index].ycor()
             if direction == "left":
                 x_left = invaders[left_index].xcor()
                 if x_left >= -280:
                     for pos_invaders in invaders:
                         position = pos_invaders.xcor()
-                        position -= speedinvaders
+                        position -= self.speedinvaders
                         pos_invaders.setx(position)
-                    x_left -= speedinvaders
+                        if (pos_invaders.xcor() - self.player.xcor() <= 20) and (pos_invaders.ycor() - self.player.ycor() <= 20):
+                            self.game_breack()
+                            self.inviders_loop = False
+                            break
+                    x_left -= self.speedinvaders
                 else:
                     y -= 40
                     direction = "right"
@@ -149,9 +169,9 @@ class jogo():
                 if x_right < 280:
                     for pos_invaders in invaders:
                         position = pos_invaders.xcor()
-                        position += speedinvaders
+                        position += self.speedinvaders
                         pos_invaders.setx(position)
-                    x_right += speedinvaders
+                    x_right += self.speedinvaders
                 else:
                     y -= 40
                     direction = "left"
@@ -162,49 +182,62 @@ class jogo():
 
             y_down = invaders[down_index].ycor()
             if y_down <= -280:
-                game_breack()
-                inviders_loop = False
+                self.game_breack()
+                self.inviders_loop = False
+                break
     def move_fire(self,fire):
         # global fire_on, x_fire, y_fire,score,jogo_runing
         if self.fire_on == False and self.jogo_runing:
-            fire_on = True
+            self.fire_on = True
             y_fire =fire.ycor()
             x_fire = fire.xcor()
-
             while y_fire < 300:
                 for inimigo in self.invaders:
                     x_ini = inimigo.xcor()
                     y_ini = inimigo.ycor()
+                   
                     if x_fire >= x_ini -20 and x_fire <= x_ini + 20:
                         if y_fire >= y_ini:
                             global left_index, right_index, down_index
                             inimigo.setx(random.randint(-280, 280))
                             inimigo.sety(random.randint(100, 250))
                             y_fire = 400
-                            left_index, right_index, down_index = init_pos()
-                            score_pen.clear()
-                            score+=1
-                            score_pen.write("Score: "+str(score), False, align="left", font=("Arial", 14, "normal"))
+                            left_index, right_index, down_index = self.init_pos()
+                            self.score_pen.clear()
+                            self.score+=1
+                            self.score_pen.write("Score: "+str(self.score), False, align="left", font=("Arial", 14, "normal"))
                             break
                 y_fire += self.firespeed
                 fire.sety(y_fire)
-                self.refresh()
+                # self.refresh()
 
             fire.hideturtle()
             fire.sety(-185)
             fire.setx(self.player.xcor())
             fire.showturtle()
             self.fire_on = False
+    def generate_inviders(self):
+        numbers_of_invaders = self.numbers_of_invaders 
+        invaders = self.invaders
 
-    def move_right():
-        x = player.xcor()
-        x += playerspeed
+        for i in range(numbers_of_invaders):
+            invaders.append(turtle.Turtle())
+
+        for enemy in self.invaders:
+            enemy.shape("invader.gif")
+            enemy.penup()
+            enemy.speed(0)
+            pos_x = random.randint(-200, 200)
+            pos_y = random.randint(100, 250)
+            enemy.setposition(pos_x, pos_y)
+    def move_right(self):
+        x = self.player.xcor()
+        x += self.playerspeed
         if x > 280:
             x = 280
-        player.setx(x)
-        if not fire_on:
-            fire.setx(x)
-
+        self.player.setx(x)
+        if not self.fire_on:
+             self.fire.setx(x)
     def move_left(self,player,fire):
         x = player.xcor()
         x -= self.playerspeed
@@ -214,10 +247,10 @@ class jogo():
         if not self.fire_on:
             fire.setx(x)
         print(fire.xcor())
-    def init_pos():
+    def init_pos(self):
         x_positions = []
         y_positions = []
-        for pos_invaders in invaders:
+        for pos_invaders in self.invaders:
             x_position = pos_invaders.xcor()
             y_position = pos_invaders.ycor()
             x_positions.append(x_position)
@@ -233,13 +266,10 @@ class jogo():
         down_id = y_positions.index(down_pos)
 
         return left_id, right_id, down_id
-    def game_breack():
-        global inviders_loop,score,jogo_runing
-        jogo_runing = False
-        score = 0
-        score_pen.clear()
-        score_pen.write("Score: "+str(score), False, align="left", font=("Arial", 14, "normal"))
-        inviders_loop = False
+    def game_breack(self):
+        self.jogo_runing = False
+        self.score = 0
+        self.inviders_loop = False
         game_over = turtle.Turtle()
         game_over.speed(0)
         game_over.color("red")
@@ -247,14 +277,14 @@ class jogo():
         game_over.setposition(0, 0)
         game_over.write("Game Over", False, align="center", font=("Arial", 30, "normal"))
         game_over.hideturtle()
-        reload = write(0,100,"")
+        reload = self.write(0,100,"")
         reload.showturtle()
         reload.shape("reload.gif")
         def reload_game(x,y):
-            global invaders,inviders_loop,move_inimigos,jogo_runing
-            inviders_loop = True
-            jogo_runing = True
-            for enemy in invaders:
+             
+            self.inviders_loop = True
+            self.jogo_runing = True
+            for enemy in self.invaders:
                 enemy.shape("invader.gif")
                 enemy.penup()
                 enemy.speed(0)
@@ -264,7 +294,7 @@ class jogo():
             reload.clear()
             reload.hideturtle()
             game_over.clear()
-            move_inimigos()
+            self.move_inimigos()
         reload.onclick(reload_game)
 
 
